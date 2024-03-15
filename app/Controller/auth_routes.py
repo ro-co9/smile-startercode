0a8dda6db242a2b1aa84f078dd9c46ec93644c7b
Arslan Ay <sarslanay@wpi.edu>
2024-03-15 15:46:18 -0400

    Starter code for SmileApp

---
+++ app/Controller/auth_routes.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+import sys
+from flask import Blueprint
+from flask import render_template, flash, redirect, url_for
+from config import Config
+
+from app import db
+
+bp_auth = Blueprint('auth', __name__)
+bp_auth.template_folder = Config.TEMPLATE_FOLDER 
+
+# @bp_auth.route('/register', methods=['GET', 'POST'])
+# def register():
---
+++ smile.py
@@ -0,0 +1,21 @@
+from app import create_app, db
+from app.Model.models import Post
+
+app = create_app()
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': app.db, 'Post': Post, 'User': User}
+
+@app.before_request
+def initDB(*args, **kwargs):
+    if app._got_first_request:
+        db.create_all()
+        # if Tag.query.count() == 0:
+        #     tags = ['funny','inspiring', 'true-story', 'heartwarming', 'friendship']
+        #     for t in tags:
+        #         db.session.add(Tag(name=t))
+        #     db.session.commit()
+
+if __name__ == "__main__":
+    app.run(debug=True)

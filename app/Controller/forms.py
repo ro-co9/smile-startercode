816ed10ac7de222353a94322b7ef40ed888cb7db
ro-co9 <rcorrea@wpi.edu>
2024-03-18 12:14:45 -0400

    Task 3.1 - Modified PostForm, Display body, happiness_level and errors.

--- app/Controller/forms.py
+++ app/Controller/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField
+from wtforms import StringField, SubmitField, SelectField, TextAreaField
 from wtforms.validators import  DataRequired, Length
 
 from app.Model.models import Post
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
+    body = TextAreaField('Body', validators=[DataRequired(), Length(min=1, max=1500)])
     happiness_level = SelectField('Happiness Level',choices = [(3, 'I can\'t stop smiling'), (2, 'Really happy'), (1,'Happy')])
     submit = SubmitField('Post')

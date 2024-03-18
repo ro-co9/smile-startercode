2533efe6586cb7449a1c983161d352aa59daf220
ro-co9 <rcorrea@wpi.edu>
2024-03-18 16:53:11 -0400

    Task 3.2 - Added '/postsmile' route. Modified 'base.html' to include create post link.

--- app/Controller/routes.py
+++ app/Controller/routes.py
@@ -1,8 +1,10 @@
 from __future__ import print_function
 import sys
 from flask import Blueprint
+from app.Controller.forms import PostForm
 from flask import render_template, flash, redirect, url_for, request
 from config import Config
+
 
 from app import db
 from app.Model.models import Post
@@ -17,3 +19,15 @@ bp_routes.template_folder = Config.TEMPLATE_FOLDER #'..\\View\\templates'
 def index():
     posts = Post.query.order_by(Post.timestamp.desc())
     return render_template('index.html', title="Smile Portal", posts=posts.all())
+
+
+@bp_routes.route('/postsmile', methods=['GET', 'POST'])
+def postsmile():
+    pform = PostForm()
+    if pform.validate_on_submit():
+            newPost = Post(title = pform.title.data, body = pform.body.data, happiness_level = pform.happiness_level.data)
+            db.session.add(newPost)
+            db.session.commit()
+            flash("Your post has been created. Post title: " + pform.title.data)
+            return redirect(url_for('routes.index'))
+    return render_template('create.html', form=pform)

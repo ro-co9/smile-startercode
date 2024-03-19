b464dae3ccb364dffa549163eee575db9c441cde
ro-co9 <rcorrea@wpi.edu>
2024-03-18 20:32:59 -0400

    Task 4.3 - Added styling to title input box, body input box, selector field.

--- app/Controller/forms.py
+++ app/Controller/forms.py
@@ -6,6 +6,6 @@ from app.Model.models import Post
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
-    body = TextAreaField('Body', validators=[DataRequired(), Length(min=1, max=1500)])
+    body = TextAreaField('Post Message', validators=[DataRequired(), Length(min=1, max=1500)])
     happiness_level = SelectField('Happiness Level',choices = [(3, 'I can\'t stop smiling'), (2, 'Really happy'), (1,'Happy')])
     submit = SubmitField('Post')
